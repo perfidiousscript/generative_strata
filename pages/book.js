d1a1e81a452ad8aa0e4d833300d6f23229404cb6
Sam Moss <sam.moss321@gmail.com>
2021-03-17 16:00:09 -0700

    adds book and models page

--- pages/book.js
+++ pages/book.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <p>Homepage</p>;
+export default function Book() {
+  return <p>Book</p>;
 }
