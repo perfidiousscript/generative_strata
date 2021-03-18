458d71b0d930cf2a3ebb48b2a246a4652cd7baeb
Sam Moss <sam.moss321@gmail.com>
2021-03-17 17:10:24 -0700

    moves icons, gets homepage title text in and styled

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,12 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <p>Homepage</p>;
+  return (
+    <>
+      <div className="container">
+        <div className="homeHeader col-md-8 offset-md-2">GENERATIVE STRATA</div>
+      </div>
+      <div className="pageBody"></div>
+    </>
+  );
 }
