86fb1f26d12126d8d0573a22dda6431fc844b15e
Sam Moss <sam.moss321@gmail.com>
2021-03-17 18:39:36 -0700

    creates and pulls in link to models page

---
+++ components/modelsLink.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function ModelsLink() {
+  return (
+    <>
+      <Link href="/models">
+        <Image src="/../public/icons/icon2.png" width={50} height={50} />
+      </Link>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import HomeLink from "../components/homeLink.js";
+import ModelsLink from "../components/modelsLink.js";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -8,8 +9,16 @@ export default function Home() {
       <div className="container">
         <div className="homeHeader col-md-8 offset-md-2">GENERATIVE STRATA</div>
       </div>
-      <div className="pageBody">
-        <HomeLink />
+      <div className="pageBody row">
+        <div className="col-md-1">
+          <HomeLink />
+        </div>
+        <div className="col-md-10">
+          <p> Some text here</p>
+        </div>
+        <div className="col-md-1">
+          <ModelsLink />
+        </div>
       </div>
     </>
   );
