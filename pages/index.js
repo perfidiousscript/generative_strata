c3442d8de6b08b4a275d9d6078bea561c27db3f7
Sam Moss <sam.moss321@gmail.com>
2021-03-17 19:59:37 -0700

    adds offset column at left of home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import HomeLink from "../components/homeLink.js";
+import BookLink from "../components/bookLink.js";
 import ModelsLink from "../components/modelsLink.js";
 import styles from "../styles/Home.module.css";
 
@@ -7,16 +8,18 @@ export default function Home() {
   return (
     <>
       <div className="container">
-        <div className="col-md-8 offset-md-2 homeHeader">GENERATIVE STRATA</div>
+        <div className="col-offset-2 col-md-10">
+          <div className={styles.homeHeader}>GENERATIVE STRATA</div>
+        </div>
       </div>
       <div className="pageBody row">
-        <div className="col-md-1">
-          <HomeLink />
+        <div className="offset-md-1 col-md-1">
+          <BookLink />
         </div>
-        <div className="col-md-10">
+        <div className="col-md-8">
           <p> Some text here</p>
         </div>
-        <div className="col-md-1">
+        <div className="col-md-2">
           <ModelsLink />
         </div>
       </div>
