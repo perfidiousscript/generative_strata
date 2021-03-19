6f2e0471e0582529d5c8fbad60c970903714ff39
Sam Moss <sam.moss321@gmail.com>
2021-03-19 14:45:26 -0700

    adds chevrons to imageviewer

--- components/imageViewer.js
+++ components/imageViewer.js
@@ -1,16 +1,32 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { Glyphicon } from "react-bootstrap";
 import styles from "../styles/ImageViewer.module.css";
+const imageSrcArray = [
+  "EXTERIOR_ANGLE",
+  "EXTERIOR_FLAT",
+  "MAJOR_1",
+  "MAJOR_2",
+  "MEDIA_1",
+  "MEDIA_2",
+  "MINOR_1",
+  "MINOR_2",
+];
 
 export default function ImageViewer() {
+  const [imageSrc, setImageSrc] = useState(imageSrcArray[0]);
+
   return (
     <div className="offset-md-2 col-md-8">
       <Image
-        src="/images/EXTERIOR_ANGLE.png"
+        src={`/images/${imageSrc}.png`}
         layout="intrinsic"
         width={800}
         height={600}
       />
+      <div className={styles.leftChevron}>&lt;</div>
+      <div className={styles.rightChevron}>&gt;</div>
     </div>
   );
 }
