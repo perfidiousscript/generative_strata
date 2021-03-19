6578010f31e5ef9f9ca28426908d9f3ad5840a62
Sam Moss <sam.moss321@gmail.com>
2021-03-19 13:03:38 -0700

    adds skeleton for ImageViewer component

---
+++ components/imageViewer.js
@@ -0,0 +1,6 @@
+import Head from "next/head";
+import styles from "../styles/ImageViewer.module.css";
+
+export default function ImageViewer() {
+  return <p>Image Viewer</p>;
+}
--- pages/book.js
+++ pages/book.js
@@ -2,12 +2,14 @@ import Head from "next/head";
 import Header from "../components/pageHeader.js";
 import HomeLink from "../components/homeLink.js";
 import ModelsLink from "../components/modelsLink.js";
+import ImageViewer from "../components/imageViewer.js";
 import styles from "../styles/Book.module.css";
 
 export default function Book() {
   return (
     <>
       <Header leftIcon={<HomeLink />} rightIcon={<ModelsLink />} />
+      <ImageViewer />
     </>
   );
 }
